a9d854e5d898e9039a535c9970798290ba9bb9c3
Ellen Hui <ellenfkh@gmail.com>
2016-10-08 22:29:52 -0700

    update the scripts to have some DECIMALS~

--- mysql-scripts.sql
+++ mysql-scripts.sql
@@ -2,14 +2,16 @@ USE oily-palm;
 
 DELIMITER //
 
+DROP PROCEDURE IF EXISTS getPhonesByDate;
+
 CREATE PROCEDURE getPhonesByDate(IN date_IN DATE)
 BEGIN
     SELECT phone
     FROM (
         SELECT village
         FROM village JOIN events
-        WHERE ABS(village.lat - events.lat) < 10
-            AND ABS(village.lon - events.lon) < 10
+        WHERE ABS(village.lat - events.lat) < 0.0667
+            AND ABS(village.lon - events.lon) < 0.0667
             AND events.date = date_IN 
         ) AS filt
         JOIN users
@@ -17,6 +19,7 @@ BEGIN
 END //
 
 
+DROP PROCEDURE IF EXISTS getEventsByPhone;
 
 CREATE PROCEDURE getEventsByPhone(IN phone_IN TEXT)
 BEGIN
@@ -29,8 +32,8 @@ BEGIN
         WHERE users.phone = phone_IN
         ) AS nearby 
     JOIN events
-    WHERE ABS(nearby.lat - events.lat) < 100
-        AND ABS(nearby.lon - events.lon) < 100
+    WHERE ABS(nearby.lat - events.lat) < 0.0667
+        AND ABS(nearby.lon - events.lon) < 0.0667
         AND events.date = CURDATE();
 END //
 
