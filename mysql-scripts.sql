325e98dcacbfb003ee14c19c9064ecf6cb60a2a3
Ellen Hui <ellenfkh@gmail.com>
2016-10-08 22:19:40 -0700

    make some stored procs

--- mysql-scripts.sql
+++ mysql-scripts.sql
@@ -1,27 +1,36 @@
 USE oily-palm; 
 
-SELECT phone
-FROM (
-    SELECT village
-    FROM village JOIN events
-    WHERE ABS(village.lat - events.lat) < 100
-        AND ABS(village.lon - events.lon) < 100
-        AND events.date = CURDATE() 
-    ) AS filt
-    JOIN users
-    ON filt.village = users.village;
+DELIMITER //
+
+CREATE PROCEDURE getPhonesByDate(IN date_IN DATE)
+BEGIN
+    SELECT phone
+    FROM (
+        SELECT village
+        FROM village JOIN events
+        WHERE ABS(village.lat - events.lat) < 10
+            AND ABS(village.lon - events.lon) < 10
+            AND events.date = date_IN 
+        ) AS filt
+        JOIN users
+        ON filt.village = users.village;
+END //
 
 
 
-SELECT nearby.phone, events.event 
-FROM (
-    SELECT users.village, village.lat, village.lon, users.phone
-    FROM users
-    JOIN village
-    WHERE village.village = users.village
-    ) AS nearby 
-JOIN events
-WHERE ABS(nearby.lat - events.lat) < 100
-    AND ABS(nearby.lon - events.lon) < 100
-    AND events.date = CURDATE();
+CREATE PROCEDURE getEventsByPhone(IN phone_IN TEXT)
+BEGIN
+    SELECT nearby.phone, events.event 
+    FROM (
+        SELECT users.village, village.lat, village.lon, users.phone
+        FROM users
+        JOIN village
+        ON village.village = users.village
+        WHERE users.phone = phone_IN
+        ) AS nearby 
+    JOIN events
+    WHERE ABS(nearby.lat - events.lat) < 100
+        AND ABS(nearby.lon - events.lon) < 100
+        AND events.date = CURDATE();
+END //
 
